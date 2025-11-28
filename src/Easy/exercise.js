81681f156dcae1924394101c32bb4e13b8694171
Mannan <mannaninamdar@gmail.com>
2025-11-28 11:51:16 +0530

    removed file

--- src/Easy/exercise.js
+++
@@ -1,9 +0,0 @@
-function argumentsLengthNew() {
-    var args = [];
-    for (var _i = 0; _i < arguments.length; _i++) {
-        args[_i] = arguments[_i];
-    }
-    return args.length;
-}
-;
-console.log(argumentsLengthNew(1, 3)); // 3
